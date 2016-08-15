dd93e00ec1b54ff6ac860e045c8dcc8ead330215
didzislauva <didzis@lauvadidzis.com>
2016-08-15 21:18:19 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='metul',
+	  version='0.1',
+	  description='The rainfall-groundwater level model',
+	  url='http://github.com/didzislauva/metul',
+	  author='LUA, Latvia University of Agriculture',
+	  author_email='didzis@lauvadidzis.com',
+	  licence='MIT',
+	  packages=['metul'],
+	  zip_safe=False)
